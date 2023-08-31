1848f5a6335fde661442aa56df760bb555136296
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-08-30 23:03:48 -0300

    Update Relogio.java

--- src/main/java/Relogio.java
+++ src/main/java/Relogio.java
@@ -20,4 +20,14 @@ public class Relogio {
         this.minuto = 0;
         this.segundo = 0;
     }
+    
+    public String obterHoraFormato24h() {
+        return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+    }
+
+    public String obterHoraFormatoAMPM() {
+        String periodo = (hora < 12) ? "AM" : "PM";
+        int horaFormato12h = (hora == 0 || hora == 12) ? 12 : hora % 12;
+        return String.format("%02d:%02d:%02d %s", horaFormato12h, minuto, segundo, periodo);
+    }
 }
