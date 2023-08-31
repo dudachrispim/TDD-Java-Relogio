daad49322cba74d9746920ff3ba53dcbe6e71540
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-08-30 23:05:59 -0300

    Update Relogio.java

--- src/main/java/Relogio.java
+++ src/main/java/Relogio.java
@@ -30,4 +30,32 @@ public class Relogio {
         int horaFormato12h = (hora == 0 || hora == 12) ? 12 : hora % 12;
         return String.format("%02d:%02d:%02d %s", horaFormato12h, minuto, segundo, periodo);
     }
+    
+    private int horaInicio;
+    private int minutoInicio;
+    private int segundoInicio;
+    private boolean marcacaoIniciada;
+
+    public void iniciarMarcacao() {
+        horaInicio = hora;
+        minutoInicio = minuto;
+        segundoInicio = segundo;
+        marcacaoIniciada = true;
+    }
+
+    public void avancarTempo(int horas, int minutos, int segundos) {
+        if (marcacaoIniciada) {
+            int totalSegundosInicio = horaInicio * 3600 + minutoInicio * 60 + segundoInicio;
+            int totalSegundosAvanco = horas * 3600 + minutos * 60 + segundos;
+            int totalSegundosDecorridos = totalSegundosInicio + totalSegundosAvanco;
+            
+            hora = totalSegundosDecorridos / 3600;
+            minuto = (totalSegundosDecorridos % 3600) / 60;
+            segundo = totalSegundosDecorridos % 60;
+        }
+    }
+
+    public String obterTempoDecorrido() {
+        return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+    }
 }
