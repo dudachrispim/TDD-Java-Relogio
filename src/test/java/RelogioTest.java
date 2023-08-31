94db3736282ae881f689097aa7b045933756c4df
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-08-30 23:03:24 -0300

    Update RelogioTest.java

--- src/test/java/RelogioTest.java
+++ src/test/java/RelogioTest.java
@@ -16,4 +16,22 @@ public class RelogioTest {
         relogio.reiniciarParaMeiaNoite();
         assertEquals("00:00:00", relogio.obterHora());
     }
+    
+    @Test
+    public void testObterHoraFormato24h() {
+        Relogio relogio = new Relogio();
+        relogio.setHora(13);
+        relogio.setMinuto(30);
+        relogio.setSegundo(45);
+        assertEquals("13:30:45", relogio.obterHoraFormato24h());
+    }
+
+    @Test
+    public void testObterHoraFormatoAMPM() {
+        Relogio relogio = new Relogio();
+        relogio.setHora(13);
+        relogio.setMinuto(30);
+        relogio.setSegundo(45);
+        assertEquals("01:30:45 PM", relogio.obterHoraFormatoAMPM());
+    }
 }
